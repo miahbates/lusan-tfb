0a702b20c35d39fa5c938bf1e65526aad22e29bd
orian <77785778+OrianP@users.noreply.github.com>
2022-03-22 17:06:18 +0000

    Add ProductCard to return statement
    - Map over products and create li with ProductCard for each products
    - Not currently rendering but also not erroring
    
    Relates #11

--- components/ProductDisplay.jsx
+++ components/ProductDisplay.jsx
@@ -1,11 +1,26 @@
 import Link from "next/link";
 import React from "react";
+import products from "../database/products";
+import ProductCard from "./ProductCard";
 
 export default function ProductDisplay() {
   return (
     <div className="box">
       <h2>Product Name</h2>
       <p>Display all products here</p>
+      <ul>
+        {products &&
+          products.map((product) => {
+            <li key={product.variety}>
+              <ProductCard
+                category={product.category}
+                subCategory={product.subCategory}
+                variety={product.variety}
+                img={product.imgs[0]}
+              ></ProductCard>
+            </li>;
+          })}
+      </ul>
     </div>
   );
 }
