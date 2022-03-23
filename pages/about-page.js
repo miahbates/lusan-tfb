35226a175621057e2b1336a93850bcf5d36b7aa6
MillyP <millypalmer1@hotmail.co.uk>
2022-03-23 17:57:56 +0000

    Styled about page, placeholder Image ready

--- pages/about-page.js
+++ pages/about-page.js
@@ -1,8 +1,4 @@
-// import Head from "next/head";
 // import Image from "next/image";
-// import StyledButton from "../components/styled-components/StyledButton";
-// import Layout from "../components/layout";
-// import StyledLink from "../components/styled-components/StyledLink";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -13,19 +9,32 @@ import {
 
 export default function AboutPage() {
   return (
-    <div>
+    <div className="about">
       <h1>About Us</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim
-        veniam, quis nostrud htyehfki miahsb Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. incididunt ut.
-      </p>
+      {/* <Image
+        src=""
+        alt="seeds"
+        width={200}
+        height={200}
+      /> */}
+      <div className="text-box">
+        <p className="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad
+          minim veniam, quis nostrud htyehfki miahsb Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. incididunt ut.
+        </p>
+      </div>
       <h2>Our Values</h2>
-      <FontAwesomeIcon icon={faSeedling} className="icon"></FontAwesomeIcon>
-      <FontAwesomeIcon icon={faEarthEurope} className="icon"></FontAwesomeIcon>
-      <FontAwesomeIcon icon={faHandshake} className="icon"></FontAwesomeIcon>
+      <div className="logos">
+        <FontAwesomeIcon icon={faSeedling} className="icon"></FontAwesomeIcon>
+        <FontAwesomeIcon
+          icon={faEarthEurope}
+          className="icon"
+        ></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faHandshake} className="icon"></FontAwesomeIcon>
+      </div>
       <Link href={"/"}>
         <a>⏎ Back to home</a>
       </Link>
