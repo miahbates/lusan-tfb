b8e9892f70387dc45588ebdda8632805800b7c69
MillyP <millypalmer1@hotmail.co.uk>
2022-03-24 13:04:03 +0000

    Imageworking, styled

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -5,19 +5,24 @@ import {
   faFacebook,
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Contact() {
   return (
     <div className="contact">
-      {/* <Image src="/grow.jpg" alt="grow" width={200} height={200} /> */}
-      <p>About Us</p>
+      <Image
+        className="grow"
+        src="/grow.jpg"
+        alt="grow"
+        width={200}
+        height={200}
+      />
+      <p>Contact Us</p>
       <a href="https://www.instagram.com/growlusan/">
         <FontAwesomeIcon icon={faInstagram} className="icon"></FontAwesomeIcon>
       </a>
       <FontAwesomeIcon icon={faLinkedin} className="icon"></FontAwesomeIcon>
       <FontAwesomeIcon icon={faFacebook} className="icon"></FontAwesomeIcon>
-      <p>Contact</p>
       <a href="mailto:rachel@growlusan.com">
         <FontAwesomeIcon icon={faEnvelope} className="icon"></FontAwesomeIcon>
         <p>lusan@lusan.com</p>
