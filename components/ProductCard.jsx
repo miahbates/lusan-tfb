f9224d50c122c5422e79905264c727d82eb2da91
miahbates <miahbates@icloud.com>
2022-03-28 22:04:53 +0100

    Styled product cards for desktop
    * Used grids/flex to organise product cards
    
    Relates #42

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -22,49 +22,47 @@ export default function ProductCard({
   // set state to toggle heart icon
 
   return (
-    <div>
-      <StyledProductCard>
-        <img alt={subCategory} src={img} />
-        <div className="info-container">
-          <div className="title-wishlist">
-            <h2>
-              {variety} {subCategory}
-            </h2>
-            <WishlistToggle></WishlistToggle>
-          </div>
-          <p>From £{price}</p>
-          <div className="tag-container">
-            {typeTags &&
-              typeTags.map((tag) => (
-                <span key={tag} className="type-tag">
-                  {tag.replace(/^\w/, (c) => c.toUpperCase())}
-                </span>
-              ))}
-            {climateTags &&
-              climateTags.map((tag) => (
-                <span key={tag} className="climate-tag">
-                  {tag.replace(/^\w/, (c) => c.toUpperCase())}
-                </span>
-              ))}
-            {waterTags &&
-              waterTags.map((tag) => (
-                <span key={tag} className="water-tag">
-                  {tag.replace(/^\w/, (c) => c.toUpperCase())}
-                </span>
-              ))}
-            {soilTags &&
-              soilTags.map((tag) => (
-                <span key={tag} className="soil-tag">
-                  {tag.replace(/^\w/, (c) => c.toUpperCase())}
-                </span>
-              ))}
-          </div>
-
-          <Link href={`/products/${variety}`} key={variety} passHref>
-            <StyledLink>More info</StyledLink>
-          </Link>
+    <StyledProductCard>
+      <img alt={subCategory} src={img} />
+      <div className="info-container">
+        <div className="title-wishlist">
+          <h2>
+            {variety} {subCategory}
+          </h2>
+          <WishlistToggle></WishlistToggle>
         </div>
-      </StyledProductCard>
-    </div>
+        <p>From £{price}</p>
+
+        <div className="tag-container">
+          {typeTags &&
+            typeTags.map((tag) => (
+              <span key={tag} className="type-tag">
+                {tag.replace(/^\w/, (c) => c.toUpperCase())}
+              </span>
+            ))}
+          {climateTags &&
+            climateTags.map((tag) => (
+              <span key={tag} className="climate-tag">
+                {tag.replace(/^\w/, (c) => c.toUpperCase())}
+              </span>
+            ))}
+          {waterTags &&
+            waterTags.map((tag) => (
+              <span key={tag} className="water-tag">
+                {tag.replace(/^\w/, (c) => c.toUpperCase())}
+              </span>
+            ))}
+          {soilTags &&
+            soilTags.map((tag) => (
+              <span key={tag} className="soil-tag">
+                {tag.replace(/^\w/, (c) => c.toUpperCase())}
+              </span>
+            ))}
+        </div>
+      </div>
+      <Link href={`/products/${variety}`} key={variety} passHref>
+        <StyledLink>More info</StyledLink>
+      </Link>
+    </StyledProductCard>
   );
 }
--- components/styled-components/StyledProductCard.jsx
+++ components/styled-components/StyledProductCard.jsx
@@ -6,7 +6,11 @@ import styled from "styled-components";
 const StyledProductCard = styled.div`
   border: 4px solid var(--mid-color);
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-  margin: 2rem;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
 
   .title-wishlist {
     display: flex;
@@ -16,18 +20,20 @@ const StyledProductCard = styled.div`
 
   .info-container {
     padding: 1rem 1rem 1.5rem 1rem;
+    flex: 1;
   }
 
   img {
-    max-width: 100%;
+    width: 100%;
     display: block;
     border-radius: 2px;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    height: 200px;
   }
 
   h2 {
     font-family: var(--sora-font);
-    font-size: 1.2rem;
+    font-size: 1.1rem;
   }
 
   .tag-container {
@@ -39,8 +45,8 @@ const StyledProductCard = styled.div`
 
   span {
     margin: 0.125rem;
-    font-family: var(--raj-font);
-    font-size: 1rem;
+    font-family: var(--sora-font);
+    font-size: 0.9rem;
   }
 
   p {
@@ -51,32 +57,16 @@ const StyledProductCard = styled.div`
 
   a {
     padding: 0.5rem;
-    margin-left: 0.25rem;
+    margin: 1rem;
+    font-size: 1.1rem;
+    font-family: var(--sora-font);
+  }
+
+  @media screen and (max-width: 600px) {
+    .product-grid {
+      grid-template-columns: repeat(1, 100%);
+    }
   }
 `;
 
 export default StyledProductCard;
-
-// display: flex;
-//   background-color: var(--light-secondary-color);
-//   border: 1px solid var(--dark-color);
-
-//   img {
-//     max-width: 100%;
-//     display: block;
-//   }
-
-//   .product-img-container {
-//     width: 30%;
-//   }
-
-//   .tag-container {
-//     display: flex;
-//     flex-wrap: wrap;
-//     width: 100%;
-//   }
-
-//   .info-container {
-//     padding-left: 15px;
-//     max-width: 80%;
-//   }
