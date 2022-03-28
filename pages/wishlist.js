8b08566fe42666f79140c2453b7a9fac81bfc5e6
orian <77785778+OrianP@users.noreply.github.com>
2022-03-28 18:12:11 +0100

    Share wishlist state with context
    - Create WishListContext file
    - Wrap layout in WishListProvider
    - Initialise state with useWishListContext in all-seeds and wishlist
    
    Relates #49 #60

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -5,16 +5,31 @@ import StyledLink from "../components/styled-components/StyledLink";
 import StyledProductCard from "../components/styled-components/StyledProductCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+import {
+  useWishListContext,
+  WishListContext,
+} from "../components/context/WishListContext";
 
 export default function Wishlist() {
+  const [wishList, setWishList] = useWishListContext(WishListContext);
+
+  useEffect(() => {
+    setWishList(() => {
+      const saved = JSON.parse(localStorage.getItem("wishlist"));
+      return saved || [];
+    });
+  }, [setWishList]);
+
+  useEffect(() => console.log("wishlist in wishlist page", wishList));
   return (
     <div className="wishlist">
       <h1>Wishlist</h1>
       <div className="emptyBox">Your wishlist is empty</div>
-      <StyledWishListItem>
+      {/* <StyledWishListItem>
         <StyledProductCard></StyledProductCard>
         <FontAwesomeIcon icon={faTrashCan} className="icon"></FontAwesomeIcon>
-      </StyledWishListItem>
+      </StyledWishListItem> */}
 
       {/* <StyledLink>
         <Link href={"/compare"}>
@@ -22,9 +37,7 @@ export default function Wishlist() {
         </Link>
       </StyledLink> */}
       <StyledLink>
-        <Link href={"/all-seeds"}>
-          <a>Back to Search</a>
-        </Link>
+        <Link href={"/all-seeds"}>Back to Search</Link>
       </StyledLink>
     </div>
   );
