e0f9f2d82b7d70ccf68908abe5792429bde34900
miahbates <miahbates@icloud.com>
2022-03-29 12:35:37 +0100

    Styling nav bar
    * now responsive
    * changed font
    * aligned icons
    * centered search and wishlist icon
    
    Relates #56

--- components/styled-components/StyledNavLink.jsx
+++ components/styled-components/StyledNavLink.jsx
@@ -4,19 +4,21 @@ const StyledNavLink = styled.ul`
   display: flex;
   flex-flow: row nowrap;
   list-style: none;
-  border-radius: 5px;
+  border-radius: 2px;
   color: var(--dark-color);
 
   li {
-    padding: 0 1rem;
-    margin: 0 2rem;
     justify-content: flex-start;
     transition: transform 0.3s ease0-in-out;
+    font-family: var(--sora-font);
+    font-size: 1.1rem;
   }
   li:hover {
     color: var(--dark-secondary-color);
-    transform: scale(1.05);
+    transform: scale(1.025);
+    transition: all 400ms;
     cursor: pointer;
+    border-bottom: 2px solid var(--dark-secondary-color);
   }
 
   @media (max-width: 768px) {
@@ -26,12 +28,21 @@ const StyledNavLink = styled.ul`
     position: fixed;
     top: -1rem;
     left: 0;
-    height: 50vh;
+    height: 70vh;
     width: 300px;
-    padding: 3.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 1rem;
     transform: ${({ open }) => (open ? "translateX(0)" : "translateX(1000%)")};
     transition: transform 0.3s ease0-in-out;
-    box-shadow: 1px 1px 1px var(--dark-secondary-color);
+    box-shadow: var(--dark-secondary-color) 0px 8px 24px;
+  }
+
+  li {
+    padding: 0.5rem 1rem;
+    margin: 0 2rem 0 0.5rem;
   }
 `;
 
