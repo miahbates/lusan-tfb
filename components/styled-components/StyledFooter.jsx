f2f255809bd4645dfa27f3b36e34fdb6ec575dab
MillyP <millypalmer1@hotmail.co.uk>
2022-03-30 11:58:47 +0100

    Footer layout reorganised

--- components/styled-components/StyledFooter.jsx
+++ components/styled-components/StyledFooter.jsx
@@ -2,16 +2,28 @@ import styled from "styled-components";
 
 const StyledFooter = styled.footer`
   background-color: var(--dark-color);
-  position: sticky;
   bottom: 0;
   width: 100%;
-  margin-top: 3rem;
   padding-bottom: 1rem;
   font-size: 1rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
+  flex-direction: row;
+
+  @media screen and (min-width: 425px) {
+    .footer {
+      flex-direction: column;
+
+      &:hover {
+        color: var(--mid-color);
+        transition: 200ms ease-in;
+      }
+    }
+    .span {
+      font-size: 0.5rem;
+    }
+  }
 
   .footer-about {
     display: flex;
@@ -23,9 +35,16 @@ const StyledFooter = styled.footer`
   }
 
   .footer-contact {
+    color: white;
     text-decoration: none;
     text-align: center;
     font-size: 1rem;
+    padding: 1rem;
+
+    &:hover {
+      color: var(--mid-color);
+      transition: 200ms ease-in;
+    }
   }
 
   .footer-links {
@@ -41,10 +60,11 @@ const StyledFooter = styled.footer`
     }
   }
 
-  p {
-    font-size: 1rem;
+  span {
+    font-size: 0.9rem;
     color: white;
     font-weight: bold;
+    padding: 0.5rem;
     &:hover {
       color: var(--mid-color);
       transition: 200ms ease-in;
