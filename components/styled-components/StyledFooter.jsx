8c7a11a1198da20c99e60f51f2f636a59fa679d5
MillyP <millypalmer1@hotmail.co.uk>
2022-03-30 12:28:11 +0100

    overlap fix

--- components/styled-components/StyledFooter.jsx
+++ components/styled-components/StyledFooter.jsx
@@ -4,16 +4,35 @@ const StyledFooter = styled.footer`
   background-color: var(--dark-color);
   bottom: 0;
   width: 100%;
-  padding-bottom: 1rem;
-  font-size: 1rem;
+  padding-top: 1rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: row;
+  flex-direction: column;
+  &:hover {
+    color: var(--mid-color);
+    transition: 200ms ease-in;
+  }
 
-  @media screen and (min-width: 425px) {
+  @media screen and (max-width: 768px) {
     .footer {
-      flex-direction: column;
+      flex-direction: row;
+      padding-top: 0.5rem;
+
+      &:hover {
+        color: var(--mid-color);
+        transition: 200ms ease-in;
+      }
+    }
+    .span {
+      font-size: 0.5rem;
+    }
+  }
+
+  @media screen and (max-width: 320px) {
+    .footer {
+      flex-direction: row;
+      padding-top: 0.5rem;
 
       &:hover {
         color: var(--mid-color);
@@ -26,10 +45,6 @@ const StyledFooter = styled.footer`
   }
 
   .footer-about {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: row;
     text-decoration: none;
     text-align: center;
   }
