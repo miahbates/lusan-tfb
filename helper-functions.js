aae707f1e54da11d9fd3c2b30514a503aab97b19
orian <77785778+OrianP@users.noreply.github.com>
2022-03-31 14:54:41 +0100

    Add generateTags() and findContent() functions
    - Move from other files into helper-functions
    
    Relates #78

--- helper-functions.js
+++ helper-functions.js
@@ -1,3 +1,6 @@
+import products from "./database/products";
+
+// local storage
 function saveToLocalStorage(key, product) {
   // get what is currently in local storage
   const currentLocalStorage = JSON.parse(localStorage.getItem(key)) || [];
@@ -17,9 +20,34 @@ function removeFromLocalStorage(key, variety) {
   return filteredLocalStorage;
 }
 
-// get from local storage
 function getFromLocalStorage(key) {
   const saved = JSON.parse(localStorage.getItem(key));
   return saved || [];
 }
-export { saveToLocalStorage, removeFromLocalStorage, getFromLocalStorage };
+
+// database tags
+// returns an array of product tags containing the keys which have a 'true' value
+function generateTags(obj) {
+  const keys = Object.keys(obj);
+  return keys.filter((key) => {
+    if (obj[key]) {
+      return key;
+    }
+  });
+}
+
+// find product by variety
+function findContent(capitalisedVariety) {
+  const foundObject = products.find((productObject) => {
+    return productObject.variety === capitalisedVariety;
+  });
+  return foundObject;
+}
+
+export {
+  saveToLocalStorage,
+  removeFromLocalStorage,
+  getFromLocalStorage,
+  generateTags,
+  findContent,
+};
